0b129197af532fe4b416dfa7251c546e58ad91ba
Ícaro Rangel <icarogin2@gmail.com>
2024-11-17 20:42:42 -0400

    first commit
    App inicial

---
+++ FinTrack/FinTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinTrack
+//
+//  Created by icaro rangel on 17/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinTrack/FinTrack/FinTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinTrackApp.swift
+//  FinTrack
+//
+//  Created by icaro rangel on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FinTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
